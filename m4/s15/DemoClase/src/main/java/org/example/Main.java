9c12b373c46c66a62b1a5f2ed43fd8cf5f6c4273
Critopher Diaz <cristopherdiaz.c@hotmail.com>
2024-10-22 20:03:28 -0300

    add: class sesion 15 module 4

--- m4/s15/DemoClase/src/main/java/org/example/Main.java
+++ m4/s15/DemoClase/src/main/java/org/example/Main.java
@@ -1,7 +1,55 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // URL de la API
+        String apiUrl = "https://cl.dolarapi.com/v1/cotizaciones/usd";
+        try {
+
+            // Crear objeto URL
+            URL url = new URL(apiUrl);
+
+            // Abrir conexión
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Configurar el método de la solicitud
+            connection.setRequestMethod("GET");
+
+            // Obtener el código de respuesta (por ejemplo, 200 para éxito)
+            int responseCode = connection.getResponseCode();
+            System.out.println("Código de respuesta: " + responseCode);
+
+            // Si la respuesta es exitosa (código 200)
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Leer la respuesta de la API
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+
+                // Leer línea por línea
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+
+                // Cerrar el buffer
+                in.close();
+
+                // Imprimir la respuesta
+                System.out.println("Respuesta de la API: " + content.toString());
+            } else {
+                System.out.println("Error en la solicitud a la API");
+            }
+
+            // Cerrar la conexión
+            connection.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
